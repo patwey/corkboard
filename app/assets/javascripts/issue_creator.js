2419c7fbbf9b83e8d6057a0c8a4b879f96e6fca7
Pat Wey <patrickwey@gmail.com>
2016-01-15 17:17:36 -0700

    Implement functionality to create issues

---
+++ app/assets/javascripts/issue_creator.js
@@ -0,0 +1,26 @@
+function createIssue() {
+  $("#submit-issue").on("click", function() {
+    var repoName = $('#repo-name').html();
+
+    $.get("/current", function(currentUser) {
+      var url = "https://api.github.com/repos/" + repoName + "/issues?access_token=" + currentUser.token;
+      var title = $("#issue-title").val();
+      var body = $("#issue-description").val();
+      var issueParams = '{"title":"'+ title +'", "body":"'+ body +'"}';
+
+      $.ajax({
+        type: "POST",
+        url:  url,
+        data: issueParams,
+        success: function(newIssue) {
+          renderIssue(newIssue);
+          $("issue-title").val("");
+          $("issue-description").val("");
+        },
+        error: function(xhr) {
+          console.log(xhr.responseText);
+        }
+      })
+    })
+  })
+}
