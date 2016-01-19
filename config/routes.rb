f220394089d686bb781c5d109daa738d9bb5c8e8
Pat Wey <patrickwey@gmail.com>
2016-01-19 13:32:16 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "welcome#index"
 
   post "auth/github", as: :login
-  get "/auth/github/callback", to: "sessions#create"
+  get "auth/github/callback", to: "sessions#create"
   get "/logout", as: :logout, to: "sessions#destroy"
 
   get "/current", to: "users#current"
