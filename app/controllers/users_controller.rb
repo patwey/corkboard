39a3b12c830d08777a7fb05f69033ddc35e49c12
Pat Wey <patrickwey@gmail.com>
2016-01-19 11:10:17 -0700

    Add favicon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def current
-    render json: { name:     current_user.name,
-                   username: current_user.username,
-                   token:    current_user.token }
+    if current_user
+      render json: { name:     current_user.name,
+                     username: current_user.username,
+                     token:    current_user.token }
+    end
   end
 end
