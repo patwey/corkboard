1223b3defd87ed2b4e5f82ca82353b045c21d662
Pat Wey <patrickwey@gmail.com>
2016-01-20 14:18:54 -0700

    Implement reverting issues
    * Issues revert to position in column unless dropped in another column
    * Issues are removed from the DOM when they are dropped in a new column

--- app/assets/javascripts/issue_drag_and_drop.js
+++ app/assets/javascripts/issue_drag_and_drop.js
@@ -1,27 +1,45 @@
 function draggableIssues() {
   $(".issue").draggable({
     containment: $(".cards"),
-    stack: ".cards"
+    stack: ".cards",
   });
 
   $("#in-progress").droppable({
     accept: ".issue",
     drop: function(event, ui) {
-      toInProgress(ui.draggable);
+      if (ui.draggable.parent().attr("id") == "in-progress") {
+        $(ui.draggable).draggable("option", "revert", true);
+      } else {
+        $(ui.draggable).draggable("option", "revert", false);
+        ui.draggable.remove();
+        toInProgress(ui.draggable);
+      }
     }
   });
 
   $("#backlog").droppable({
     accept: ".issue",
     drop: function(event, ui) {
-      toBacklog(ui.draggable);
+      if (ui.draggable.parent().attr("id") == "backlog") {
+        $(ui.draggable).draggable("option", "revert", true);
+      } else {
+        $(ui.draggable).draggable("option", "revert", false);
+        ui.draggable.remove();
+        toBacklog(ui.draggable);
+      }
     }
   });
 
   $("#complete").droppable({
     accept: ".issue",
     drop: function(event, ui) {
-      toComplete(ui.draggable);
+      if (ui.draggable.parent().attr("id") == "complete") {
+        $(ui.draggable).draggable("option", "revert", true);
+      } else {
+        $(ui.draggable).draggable("option", "revert", false);
+        ui.draggable.remove();
+        toComplete(ui.draggable);
+      }
     }
   });
 }
