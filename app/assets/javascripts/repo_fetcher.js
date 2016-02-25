07e33b95b8337194c95857c73fc066e71f427735
Pat Wey <patrickwey@gmail.com>
2016-02-25 15:50:02 -0700

    Refactor and project_fetcher and rename to repo_fetcher

---
+++ app/assets/javascripts/repo_fetcher.js
@@ -0,0 +1,28 @@
+function fetchRepos() {
+  $.get("/current", function(user) {
+    fetchReposForUser(user);
+  });
+}
+
+function fetchReposForUser(user) {
+  var userReposEndpoint = "https://api.github.com/user/repos?access_token=" + user.token + "&visibility=public&affiliation=owner&sort=updated";
+
+  $.get(userReposEndpoint, function(repos) {
+    hideSpinners();
+    renderValidRepos(repos);
+  });
+}
+
+function hideSpinners() {
+  $(".spinner").hide();
+}
+
+function renderValidRepos(repos) {
+  $.each(repos, function(idx, repo) {
+    if (isValidRepo) { renderProject(repo); }
+  });
+}
+
+function isValidRepo(repo) {
+  return !repo.fork;
+}
