c56fa3e2fa2744cf643abb2c0db356dbbaa69806
Pat Wey <patrickwey@gmail.com>
2016-02-27 16:37:25 -0700

    WIP

--- app/assets/javascripts/issue_fetcher.js
+++ app/assets/javascripts/issue_fetcher.js
@@ -2,15 +2,16 @@ function fetchIssues() {
   var repoName  = $('#repo-name').html();
   var repos_url = "https://api.github.com/repos/"+ repoName +"/issues?state=all";
 
-  $.get(repos_url, function(issues) {
-    $.each(issues, function(idx, issue) {
-      if (!issue.pull_request) {
-        renderIssue(issue, "append");
-      }
-    });
-  })
+  $.get(repos_url, function(issues) { appendValidIssues(issues); })
+   .always(function() { draggableIssues(); });
+}
 
-  .always(function() {
-    draggableIssues();
+function appendValidIssues(issues) {
+  $.each(issues, function(idx, issue) {
+    if (isValidIssue(issue)) { renderIssue(issue, "append"); }
   });
+}
+
+function isValidIssue(issue) {
+  return !issue.pull_request;
 }
--- app/assets/javascripts/issue_renderer.js
+++ app/assets/javascripts/issue_renderer.js
@@ -1,68 +1,67 @@
 function renderIssue(issue, option) {
-  var labels = issue.labels.map(function(label) {
-    return label.name;
-  });
-  var issueType = getIssueType(issue, labels);
-  var selector = issueSelector(issueType);
+  var labelNames = pluckLabelNames(issue.labels);
+
+  var label = issueLabel(labelNames);
+  var columnId = getColumnId(issue, labelNames);
+  var body = sanitizeIssueBody(issue.body);
 
   if (option == "prepend") {
-    $(selector).prepend(issueTemplate(issue, labels));
+    $(columnId).prepend(issueTemplate(issue, body, label, issue.number, issue.title));
   } else {
-    $(selector).append(issueTemplate(issue, labels));
+    $(columnId).append(issueTemplate(issue, body, label, issue.number, issue.title));
   }
 }
 
-function getIssueType(issue, labels) {
-  if (issue.state == "closed") {
-    return "complete";
-  } else if (labels.includes("in progress")) {
-    return "in progress";
-  } else {
-    return "backlog";
+function pluckLabelNames(labels) {
+  return labels.map(function(label) { return label.name; });
+}
+
+function issueLabel(labelNames) {
+  var labelNodeData = getLabelNodeData(labelNames) // returns [Label Text, Label Style]
+  return getLabelDOMNode(labelNodeData);
+}
+
+function getLabelNodeData(labels) {
+  switch (labels) {
+    case labels.includes("help wanted"):
+      return ["Help Wanted", "label-success"];
+      break;
+    case labels.includes("enhancement"):
+      return ["Enhancement", "label-info"];
+      break;
+    case labels.includes("bug"):
+      debugger
+      return ["Bug", "label-danger"];
+      break;
   }
 }
 
-function issueTemplate(issue, labels) {
-  var body     = issueBody(issue);
-  var label    = issueLabel(labels);
+function getLabelDOMNode(labelNodeData) {
+  if (labelNodeData) {
+    var labelText = labelNodeData[0];
+    var labelStyle = labelNodeData[1];
 
-  return "<div class='issue panel panel-warning ui-widget-content'>" +
-         "<div class='panel-heading'>" +
-         "<span class='issue-number'>" + issue.number + "</span>" + " - " + issue.title +
-         "</div>" +
-         "<div class='panel-body'>" +
-         "<span>" +
-         body +
-         "</span>" +
-         label +
-         "</div>"+
-         "</div>"
+    return "<div class='pull-right'><span class='label" + labelStyle + "label-success'>" +
+           labelText + "</span></div>"
+  } else {
+    return ""
+  }
 }
 
-function issueSelector(type) {
-  var selectors = { "in progress":"#in-progress",
-                    "backlog":"#backlog",
-                    "complete":"#complete" };
-
-  return selectors[type];
-}
-
-function issueBody(issue) {
-  if (issue.body) {
-    return issue.body;
+function sanitizeIssueBody(body) {
+  if (body) {
+    return body;
   } else {
     return "";
   }
 }
 
-function issueLabel(labels) {
-  if (labels.includes("help wanted")) {
-    return "<div class='pull-right'><span class='label label-success'>Help Wanted</span></div>"
-  } else if (labels.includes("enhancement")) {
-    return "<div class='pull-right'><span class='label label-info'>Enhancement</span></div>"
-  } else if (labels.includes("bug")) {
-    return "<div class='pull-right'><span class='label label-danger'>Bug</span></div>"
+function getColumnId(issue, labels) {
+  if (issue.state == "closed") {
+    return "#complete";
+  } else if (labels.includes("in progress")) {
+    return "#in-progress";
   } else {
-    return ""
+    return "#backlog";
   }
 }
